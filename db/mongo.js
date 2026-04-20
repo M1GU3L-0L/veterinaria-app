ca4f1c6bf3b1480ef3fa9c59cac084732132b3d0
JuanBejarano-dev <juan.bejarano16.dev@gmail.com>
2026-04-19 20:43:04 -0500

    Cambio en mongo.js de la conexion

--- db/mongo.js
+++ db/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_PUBLIC_URL)
   .then(() => console.log("MongoDB conectado"))
   .catch(err => console.log(err));
