ad2f583b54d38010d6f7489e32f92a6e9fe4100f
JuanBejarano-dev <juan.bejarano16.dev@gmail.com>
2026-04-19 21:30:48 -0500

    debug usuarios

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 require("./db/mongo");
 require("./db/postgres");
+const Usuario = require("./models/Usuario");
 
 
 const app = express();
@@ -10,12 +11,24 @@ app.use(cors());
 app.use(express.json());
 
 // rutas
+
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+    res.json(usuarios);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error obteniendo usuarios");
+  }
+});
+
 app.use("/auth", require("./routes/auth"));
 app.use("/ventas", require("./routes/ventas"));
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/login.html");
 });
+
 app.get("/crear-admin", async (req, res) => {
   const usuario = new Usuario({
     correo: "admin@vet.com",
